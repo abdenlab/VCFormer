e18f07ce2564ab4edb6c902733ce5bdd8b802bea
Garrett Ng <56460265+GarrettNg@users.noreply.github.com>
2023-08-07 17:17:33 -0400

    Use __all__ for __init__

--- vcformer/__init__.py
+++ vcformer/__init__.py
@@ -1,1 +1,8 @@
-from .vcformer import read_info_schema, read_sample_schema, read_vcf_as_pandas, read_vcf_as_polars
+from .vcformer import *
+
+__all__ = [
+    "read_info_schema",
+    "read_sample_schema",
+    "read_vcf_as_pandas",
+    "read_vcf_as_polars",
+]
