4e30abb2f5f3326472b9811ed415784b9e061cf6
Garrett Ng <56460265+GarrettNg@users.noreply.github.com>
2023-08-07 17:19:33 -0400

    Ignore ruff in init file

--- vcformer/__init__.py
+++ vcformer/__init__.py
@@ -1,6 +1,6 @@
-from .vcformer import *
+from .vcformer import *  # noqa: F403
 
-__all__ = [
+__all__ = [  # noqa: F405
     "read_info_schema",
     "read_sample_schema",
     "read_vcf_as_pandas",
