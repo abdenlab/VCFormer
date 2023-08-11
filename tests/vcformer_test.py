43974a9d7cf3d4d949528b455a2b93a0bed3fe90
Garrett Ng <56460265+GarrettNg@users.noreply.github.com>
2023-08-11 10:10:16 -0400

    Create tests (#7)

---
+++ tests/vcformer_test.py
@@ -0,0 +1,30 @@
+from vcformer import (
+    read_info_schema,
+    read_sample_schema,
+    read_vcf_as_pandas,
+    read_vcf_as_polars,
+)
+
+
+vcf_path = "tests/fixtures/DRR259113.ref.snpeff.vcf.gz"
+# index file f"{vcf_path}.tbi" is also present, but isn't required by pysam
+
+
+def test_read_info_schema():
+    df = read_info_schema(vcf_path)
+    assert not df.empty
+
+
+def test_read_sample_schema():
+    df = read_sample_schema(vcf_path)
+    assert not df.empty
+
+
+def test_read_vcf_as_pandas():
+    df = read_vcf_as_pandas(vcf_path)
+    assert not df.empty
+
+
+def test_read_vcf_as_polars():
+    df = read_vcf_as_polars(vcf_path)
+    assert not df.is_empty()
